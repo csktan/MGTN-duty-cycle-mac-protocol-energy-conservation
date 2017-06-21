6939592a58439b5a8da9d2c5c927c8f01b73eac8
chetan KC <chetan.kc@bsc.es>
2017-06-21 23:51:30 +0200

    added plot

--- project.m
+++ project.m
@@ -3,10 +3,9 @@
 % /*********** Alejandro Montero ***********/
 % /***********   Chetan KC    ***********/
 
-clear all;
+clear all; 
 % Get the time when we start computations:
 start_time = clock;
-
 % problem constants
 P     = 32;            % Payload [byte]
 R     = 31.25;         % CC2420 Radio Rate [kbyte/s]
@@ -32,24 +31,76 @@ Tcw  = 15*0.62;        % Contention window size [ms]
 Tcs  = 2.60;           % Time [ms] to turn the radio into TX and probe the channel (carrier sense)
 Tdata = Thdr + P/R + Tack; % data packet transmission duration [ms]
 %%%%%%%%%%%%%%%%%
-%
-d = 1;
-I_d = 0;
-F_I_d = Fs * (((D^2)-(d^2))/ (2*d)-1);
-F_d_out = Fs * ((((D^2)-(d^2)) + ((2*d)-1)) / (2*d)-1);
+d = 0:D;
+Tw = 100:400;
+% number of node in ring d
+if d == 0 
+    N_d = 1;
+else
+    N_d = ((2.*d)-1)*C;
+end
+% Average number of input links
+if d == D
+    I_d = 0;
+elseif d == 0
+        I_d = C;
+else
+    I_d = ((2.*d)+1)/((2.*d)-1);
+end
+%input frequency (number of packets that enter a node)
+if d == 0
+    F_I_d = Fs*D^2*C;
+else 
+    F_I_d = Fs * (((D^2)-(d.^2))/ (2.*d)-1);
+end
+% output frequecy (number of packers that leaves the packet)
+if d == D
+    F_d_out = Fs;
+else
+    F_d_out = Fs * ((((D^2)-(d.^2)) + ((2.*d)-1)) / (2.*d)-1);
+end
+% background node's traffic frequency
 F_B_d = C -((I_d)*F_d_out);
 
+% energy of n node
+Ttx = ((Tw/(Tps + Tal))*((Tps + Tal)/2)) + Tack + Tdata;
+E_cs = ((Tcs));
+E_tx = (Tcs+Tal+Ttx)*F_d_out;
+E_rx = (((3/2)* Tps)+ Tack+Tdata)*F_I_d;
+E_ovr = (((3/2)*(Ttx/Tw)) * Tps)* F_B_d;
+E_n = E_cs + E_tx + E_rx + E_ovr;
+
+% delay of n node
+n = 1:N;
+%p = (d.^n);
+Tcw  = 15*0.62; 
+
+L_n = ((Tw/2)+ (Tcw/2) + Tdata);
+
+fprintf('energy per node = %f \nlatency per node = %f\n\n',...
+    E_n, L_n)
 % network energy consumption
-F = 1;
-a1 = Tcs + Tal + ((3/2) * Tps) * (((Tps+Tal)/2) + Tack + Tdata) * F;
+a1 = Tcs + Tal + ((3/2) * Tps) * (((Tps+Tal)/2) + Tack + Tdata) * F_B_d;
 a2 = F_d_out/2;
 a3 = ((((Tps+Tal)/2) + Tcs + Tal + Tack + Tdata) * F_d_out)+(((((3/2)*Tps)+Tack+Tdata) * F_I_d) + ((3/4)*Tps*F_B_d));
-Tw = 1;
-E_xmac = ((a1/Tw) + (a2*Tw) + a3);
+E_xmac = ((a1./Tw) + (a2.*Tw) + a3);
+%[n,E_xmac] = fminunc(@(n)-E_xmac,N)
+
+% End to End delay (e2e) 
+B1 = symsum((1/2), i, 1, d.^n);
+
+B2 = (((Tcw/2) + Tdata));
+L_xmac = ((B1 * Tw) + B2);
+
+fprintf('Energy_Comsumptio of network = %f \n End_2_End_Delay = %f\n\n ',...
+    E_xmac, L_xmac)
+% test plot
+figure,
+plot(E_xmac, L_xmac);
+figure,
+plot (E_n , L_n);
 
 
-%sprintf = ('energy consumption is %f \n value of a1 %d \n',...
- %   E_xmac,a1)
-fprintf('network energy consumption = %f \na1 =  %f\na2 = %f \na3 = %f\n',...
-        E_xmac,a1,a2,a3)
-% end to end delay (e2e)
+
+
+
